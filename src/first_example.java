8e149eb2d6ac881e6da0f5cec488540240f96c6f
Viralate <dylan.geraci1@gmail.com>
2024-01-12 16:31:08 -0500

    update

---
+++ src/first_example.java
@@ -0,0 +1,51 @@
+public class first_example {
+    public static void main(String[] args) {
+        // Declare integer variables
+        int intOperandA = 15;
+        int intOperandB = 8;
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;
+
+        // Declare double variables
+        double doubleOperandA = 3.75;
+        double doubleOperandB = 4.75;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+
+        // Create arithmetic operations for integers
+        intSum = intOperandA + intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+
+        // Create arithmetic operations for decimals (doubles)
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+
+        // Print commands to print results of int
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+        System.out.println("The product using ints of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+        System.out.println("The difference using ints of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+        System.out.println("The quotient using ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+        System.out.println("The modulo using ints of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+        // Print commands to print results of double
+        System.out.println("The sum using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+        System.out.println("The product using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+        System.out.println("The difference using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+        System.out.println("The quotient using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+    }
+
+    
+
+
+
+}
---
+++ src/second_example.java
@@ -0,0 +1,27 @@
+public class second_example {
+    public static void main(String[] args) {
+
+    //declare variables
+
+    int intAge = 25;
+    int intSiblings = 1;
+    double doubleWaterPrice = 1.50;
+    String favoriteColor = "red";
+    double height = 5.7;
+    int birthYear = 1998;
+    String middleName = "Joseph";
+    
+    // process and print data
+    System.out.println("My age is: " + intAge);
+    System.out.println("I have " + intSiblings + " sibling(s)");
+    System.out.println("I paid $" + doubleWaterPrice + " for my water!");
+    System.out.println("My favorite color is: " + favoriteColor);
+    System.out.println("My height is: " + height + " feet");
+    System.out.println("My birth year is: " + birthYear);
+    System.out.println("My middle name is: " + middleName);
+
+  
+
+
+    }
+}
